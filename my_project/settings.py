c17b9826789b5fdc34223145c3b9591cc1f45604
pongpradk <pongpradk@gmail.com>
2025-05-21 16:29:45 +0700

    Update CSRF in `settings.py`

--- my_project/settings.py
+++ my_project/settings.py
@@ -172,3 +172,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
