30105ff305209f874f310f9b5f100ac2753db027
pongpradk <pongpradk@gmail.com>
2025-05-21 16:44:25 +0700

    Update `docker-run.sh` to create superuser

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -10,6 +10,15 @@ python manage.py migrate --noinput
 if [ "$DJANGO_ENV" = "development" ]; then
     python manage.py runserver 0.0.0.0:8000
 else
+    # Create Django superuser
+    if [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
+        python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+email = '$DJANGO_SUPERUSER_EMAIL';
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email, '$DJANGO_SUPERUSER_PASSWORD')"
+    fi
     # Run Gunicorn
     RUNTIME_PORT=${PORT:-8000}
     RUNTIME_HOST=${HOST:-0.0.0.0}
