248078b33f41d90d14ccb0a0b48a5f956fbb6db5
pongpradk <pongpradk@gmail.com>
2025-05-22 17:02:40 +0700

    Add /api/ in front of all backend routes

--- my_project/urls.py
+++ my_project/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path('accounts-auth/', include('rest_framework.urls')),
-    path('inventory/', include('inventory.urls')),
+    path("admin/", admin.site.urls),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/accounts-auth/", include("rest_framework.urls")),
+    path("api/inventory/", include("inventory.urls")),
 ]
