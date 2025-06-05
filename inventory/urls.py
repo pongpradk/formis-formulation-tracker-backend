c809dcc77172dee918ecd00b495670d0225e5e71
pongpradk <pongpradk@gmail.com>
2025-06-05 22:10:48 +0700

    feat: create ingredient comparison API

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('products/update/<int:pk>/', views.ProductUpdate.as_view(), name='product-update'),
     path('products/delete/<int:pk>/', views.ProductDelete.as_view(), name='product-delete'),
     path('products/<int:product_id>/ingredients/', views.IngredientList.as_view(), name='ingredient-list'),
+    path('products/<int:product_id>/compare-ingredients/', views.IngredientCompareView.as_view(), name='compare-ingredients'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import ProductSerializer, IngredientSerializer
 from .models import Product, Ingredient
 from rest_framework.permissions import IsAuthenticated
@@ -44,3 +45,23 @@ class IngredientList(generics.ListAPIView):
         product_id = self.kwargs.get('product_id')
         product = get_object_or_404(Product, id=product_id, owner=self.request.user)
         return Ingredient.objects.filter(product=product)
+
+class IngredientCompareView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id, owner=request.user)
+        
+        submitted_ingredients = request.data.get('ingredients')
+        if not submitted_ingredients:
+            return Response(
+                {'error': 'No ingredients provided'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        saved_ingredients = ''.join(product.ingredients.lower().split())
+        submitted_ingredients = ''.join(submitted_ingredients.lower().split())
+
+        match = (saved_ingredients == submitted_ingredients)
+        
+        return Response({'match': match})
